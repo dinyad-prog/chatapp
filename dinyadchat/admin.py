c2d2fd83350757909de80344c9cb89eb1574bfef
Dinyad Yeto <donatien.yeto@imsp-uac.org>
2020-04-11 17:39:56 +0100

    Mon ChatApp,premier commit

---
+++ dinyadchat/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import personne,recepteur,message
+# Register your models here.
+
+admin.site.register(personne)
+admin.site.register(recepteur)
+admin.site.register(message)
+
+
+# Register your models here.
---
+++ dinyadchat/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+class personne(models.Model):
+	nom=models.CharField(max_length=30)
+	prenom=models.CharField(max_length=30)
+	pseudo=models.CharField(max_length=1)
+	pwd=models.CharField(max_length=30)
+	amis=models.ManyToManyField("self")
+
+	def __unicode__(self):
+		return self.prenom+" "+self.nom
+		
+class recepteur(models.Model):
+	recpt=models.ForeignKey(personne,on_delete=models.CASCADE)
+
+
+class message(models.Model):
+	contenu=models.TextField()
+	date=models.DateTimeField(default=timezone.now)
+	auteur=models.ForeignKey(personne,on_delete=models.CASCADE)
+	recpt=models.ForeignKey(recepteur,on_delete=models.CASCADE)
+
+	def __unicode__(self):
+		if len(self.contenu) > 20:
+			return self.contenu[:19]+"..."
+		else:
+			return self.contenu
+
+# Create your models here.
