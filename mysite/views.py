ed701cbe135c48b8e58b67f2ce6ee281127eddb5
Dinyad Yeto <donatien.yeto@imsp-uac.org>
2020-04-12 10:34:45 +0100

    Modification5 html css

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-from django . http import HttpResponse
+from django.http import HttpResponse
 from datetime import datetime
 from django.http import HttpResponseRedirect
 from forms import *
